155c524f39374ef4149f84e4006a4b59dbe316bf
Clement <clement.ramoslage@gmail.com>
2023-03-25 16:54:14 +0100

    refactor: turn gradient descent into class

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,21 +3,29 @@ import numpy as np
 
 from metrics import Metrics
 
-def batch_gradient_descent(X, y, lr, max_iters, beta=None):
-  if beta is None:
-    beta = np.random.rand(X.shape[1])
-  else:
-    assert isinstance(beta, np.ndarray)
+class GradientDescent:
+  def __init__(self, X, y, lr):
+    self.X = X
+    self.y = y
+    self.lr = lr
 
-  # init metrics
-  losses = [Metrics.mse(X, y, beta)]
-  betas = [beta.copy()]
+    self.N, self.D = self.X.shape
 
-  for _ in range(max_iters):
-    grad = X.T.dot(X.dot(beta) - y) / X.shape[0]
-    beta -= lr * grad
-    # save metrics
-    losses.append(Metrics.mse(X, y, beta))
-    betas.append(beta.copy())
+  def batch(self, n_iter, beta=None):
+    if beta is None:
+      beta = np.random.rand(self.D)
+    else:
+      assert isinstance(beta, np.ndarray)
 
-  return np.array(betas), np.array(losses)
+    # init metrics
+    losses = [Metrics.mse(self.X, self.y, beta)]
+    betas = [beta.copy()]
+
+    for _ in range(n_iter):
+      grad = self.X.T.dot(self.X.dot(beta) - self.y) / self.N
+      beta -= self.lr * grad
+      # save metrics
+      losses.append(Metrics.mse(self.X, self.y, beta))
+      betas.append(beta.copy())
+
+    return np.array(betas), np.array(losses)
