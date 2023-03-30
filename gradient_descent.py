946efef2d65af5da4ecb0803a23ef98245526fd6
Clement <clement.ramoslage@gmail.com>
2023-03-30 09:07:29 +0200

    refactor: handle params pointer access

--- gradient_descent.py
+++ gradient_descent.py
@@ -13,19 +13,20 @@ class GradientDescent:
 
   def batch(self, n_iter, beta=None):
     if beta is None:
-      beta = np.random.rand(self.D)
+      _beta = np.random.rand(self.D)
     else:
       assert isinstance(beta, np.ndarray)
+      _beta = beta.copy()
 
     # init metrics
     losses = [Metrics.mse(self.X, self.y, beta)]
-    betas = [beta.copy()]
+    betas = [_beta]
 
     for _ in range(n_iter):
-      grad = self.X.T.dot(self.X.dot(beta) - self.y) / self.N
-      beta -= self.lr * grad
+      grad = self.X.T.dot(self.X.dot(_beta) - self.y) / self.N
+      _beta -= self.lr * grad
       # save metrics
-      losses.append(Metrics.mse(self.X, self.y, beta))
+      losses.append(Metrics.mse(self.X, self.y, _beta))
       betas.append(beta.copy())
 
     return np.array(betas), np.array(losses)
